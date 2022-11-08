b57d5402d336a93db05e17100ca4660e59fe3d7a
Jefferson Silva <jeffersonsantsil@gmail.com>
2022-11-08 17:04:32 -0300

    Primeira Aula

---
+++ adicionando-notas.js
@@ -0,0 +1,11 @@
+const notas = [10, 6.5, 7.5];
+
+console.log(notas);
+
+notas.push(7)
+console.log(notas);
+
+const media = (notas[0] + notas[1] + notas[2] + notas[3]) / 4;
+
+
+console.log(media);
---
+++ retirando-nota.js
@@ -0,0 +1,12 @@
+const notas = [10, 6.5, 7.5, 5.5, 10];
+
+console.log(notas);
+
+notas.pop()
+console.log(notas);
+
+const media = (notas[0] + notas[1] + notas[2] + notas[3]) / 4;
+
+
+console.log(`A média é: ${media}.`);
+
